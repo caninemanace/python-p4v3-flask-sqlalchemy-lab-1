a474d6ef2efa6ee05b6b2784fb0a94e6a29b8c0b
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-06-19 12:09:06 +0300

    assignment complete

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-from flask import jsonify
+
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -21,29 +21,35 @@ def index():
     return make_response(body, 200)
 
 # Add views here
-
 @app.route('/earthquakes/<int:id>')
-def get_earthquake_by_id(id):
-    earthquake = Earthquake.query.get(id)
-
-    if earthquake:
-        return jsonify(earthquake.to_dict()), 200
+def get_earthquake(id):   
+    quake = Earthquake.query.filter(Earthquake.id == id).first()
+    if quake:
+        return make_response(quake.to_dict(), 200)
     else:
         return jsonify({"message": f"Earthquake {id} not found."}), 404
-
-@app.route('/earthquakes/magnitude/<float:magnitude>')
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
 def get_earthquakes_by_magnitude(magnitude):
-    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
-    
-    quake_list = [quake.to_dict() for quake in quakes]
-    response = {
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quake_list = [
+        {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        for quake in earthquakes
+    ]
+
+    return jsonify({
         "count": len(quake_list),
         "quakes": quake_list
-    }
-    
-    return jsonify(response), 200
+    }), 200
 
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5556, debug=True)
+
+
